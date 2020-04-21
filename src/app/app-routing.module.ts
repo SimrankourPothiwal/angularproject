b8f1bc0570c7cb21ec46e3fc8863a8f89acd3cd1
SimrankourPothiwal <psimrankour1313@gmail.com>
2020-04-21 16:13:29 +0530

    added src

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { HomeComponent } from './home/home.component';
+import { NgModule, Component } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent} from './about/about.component'
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  }
+];
+
+
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
